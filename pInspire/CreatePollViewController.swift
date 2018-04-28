ca6eebccdb6fef99481eb98c9945e8ed4c26dede
Jia-Yao <jiasherryyao@gmail.com>
2018-04-28 16:09:53 -0700

    two tabs initial setup

---
+++ pInspire/CreatePollViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddPollViewController.swift
+//  pInspire
+//
+//  Created by Jia Yao on 4/28/18.
+//  Copyright © 2018 parachute. All rights reserved.
+//
+
+import UIKit
+
+class CreatePollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ pInspire/Poll.swift
@@ -0,0 +1,14 @@
+//
+//  Poll.swift
+//  pInspire
+//
+//  Created by Jia Yao on 4/28/18.
+//  Copyright © 2018 parachute. All rights reserved.
+//
+
+import Foundation
+import os.log
+
+class Poll{
+    
+}
